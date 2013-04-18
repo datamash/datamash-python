3eb166709562b7d9c7e03063cab686c4fb2a92d7
Bjoern Stiel <bjoern.stiel@gmail.com>
2013-04-18 20:40:32 +0200

    initial demo commit, need to fill with content

--- demo/datamash-connect-demo.py
+++ demo/datamash-connect-demo.py
@@ -7,17 +7,3 @@ API_KEY = 'your-api-key'
 REPOSITORY_KEY = 'your-custom-key'
 
 client = Client(API_KEY)
-
-date = datetime.datetime(2012, 1, 1)
-
-for day in range(1, 10):
-    # print out the current day we are sending data for
-    print date
-
-    data = []
-    # 1440 minutes in one day
-    for min in range (1, 1441):
-        data.append(DataPoint(date, random.random() * 50.0))
-        date = date + datetime.timedelta(minutes=1)
-
-    client.write_key(SERIES_KEY, data)
